33c7aa9576249098be14555d3b3b40950a880d43
Ryan <rygro@icloud.com>
2022-11-01 10:03:44 -0400

    ease enter the commit message for your changes. Lines starting

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ console.log(x)
 function myClick(){
     alert("It works")
 }
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
